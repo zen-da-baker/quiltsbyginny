17c7b507cf0ae93375eab8dcf882488f5157116d
Dawn <120154112+donovan-frazier@users.noreply.github.com>
2023-03-29 16:41:35 -0700

    Smooth scrolling

---
+++ JavaScript/smoothScrolling.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('a[href^="#home"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('a[href^="#quilts"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('a[href^="#contact"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
