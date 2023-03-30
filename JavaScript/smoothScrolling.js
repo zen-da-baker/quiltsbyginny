c9ad45729fe1618824a034aaa40cac78f3e428e5
Dawn <120154112+donovan-frazier@users.noreply.github.com>
2023-03-29 20:57:42 -0700

    Button and image position fix

--- JavaScript/smoothScrolling.js
+++ JavaScript/smoothScrolling.js
@@ -27,3 +27,33 @@ document.querySelectorAll('a[href^="#contact"]').forEach(anchor => {
         });
     });
 });
+
+document.querySelectorAll('location[href^="#home"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('location[href^="#quilts"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('location[href^="#contact"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
