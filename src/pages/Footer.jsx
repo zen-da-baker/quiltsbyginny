9183fe6531210b165ba2546c3b79eb0f872fc8db
Don <120154112+zen-da-baker@users.noreply.github.com>
2025-07-02 20:58:16 -0700

    Submission form uploaded to hostinger

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -9,11 +9,19 @@ export function Footer() {
 
     return (
         <footer className="footer">
+
             <p className="center-text">
-                &copy;<em id="date"></em>{year} by Virginia Frazier and Coding Cacti. All Rights Reserved.
+                Website designed by ByteSized Crew
             </p>
 
-            <p className="center-text">The Facebook logo is trademarked by its respective owner.</p>
+            <p className="center-text">
+                &copy;<em id="date"></em>{year} by Virginia Frazier. All Rights Reserved.
+            </p>
+
+            <p className="center-text">
+                The Facebook logo is trademarked by its respective owner.
+            </p>
+            
         </footer>  
     )
 }
