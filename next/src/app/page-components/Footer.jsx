40eba6d2dc9c66bf76ce0492e6d558d9f7fc2465
Don <120154112+zen-da-baker@users.noreply.github.com>
2025-10-09 23:08:50 -0700

    Link added to footer

--- next/src/app/page-components/Footer.jsx
+++ next/src/app/page-components/Footer.jsx
@@ -10,15 +10,29 @@ export function Footer() {
         <footer className="footer">
 
             <p className="center-text">
-                Website designed by <img src={ icon.src } height={ 20 } width={ 20 } alt="Orange fruit" /> <span id="bytesized">ByteSized Crew</span>
+
+                Website designed by 
+
+                <a href="https://bytesizedcrew.com" rel="noreferrer" target="_blank">
+                
+                    <img src={ icon.src } height={ 20 } width={ 20 } alt="Orange fruit" /> 
+                    
+                    <span id="bytesized">ByteSized Crew</span>
+
+                </a>
+
             </p>
 
             <p className="center-text">
-                &copy;<em id="date"></em>{ year } by Virginia Frazier. All Rights Reserved.
+
+                &copy; { year } by Virginia Frazier. All Rights Reserved.
+
             </p>
 
             <p className="center-text">
+
                 The Facebook logo is trademarked by its respective owner.
+                
             </p>
             
         </footer>  
